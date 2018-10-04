e5cbc667c640ca0bedf8cd1bd23868c0b0c0c971
Miki <miki@test.com>
2018-10-04 15:22:33 +0200

    Initial push

---
+++ GitRepoInitialTest/Controllers/ContentController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GitRepoInitialTest.Controllers
+{
+    public class ContentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+		public IActionResult Managing()
+		{
+			ViewData["Message"] = "Git - local-branching-on-the-cheap";
+
+			return View();
+		}
+
+        [HttpPost]
+        public IActionResult ManagingPOST(string searchValue)
+        {
+            string test = HelperRepository.HelperSaveToGit.GitRepo();
+
+            return View("Managing");
+        }
+    }
+}
